34c1870c531bf50456c189e5e2fe17c54b297741
Cristopher Vera <cristopheryulian.vera.contractor@bbva.com>
2024-07-22 14:35:16 -0500

    22/07/2024 se limpia archivo y unicamente se dejan los campos necesarios para E2E performance

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-'''
-se importa libreria pandas para poder crear data frame y tratar los datos de la hoja origen
-'''
 import pandas as pd
 
-'''
-funcion separar_datos_excel se usa para tomar la hoja de origen y separarlo por comas
-'''
 def separar_datos_excel(archivo_excel, hoja_origen, hoja_destino):
     df = pd.read_excel(archivo_excel, sheet_name=hoja_origen, header=None)
 
@@ -16,6 +10,13 @@ def separar_datos_excel(archivo_excel, hoja_origen, hoja_destino):
     columnas_nuevas = {i: f'Col{i + 1}' for i in range(len(df_separado.columns))}
     df_separado.rename(columns=columnas_nuevas, inplace=True)
 
+    # Eliminar las columnas Col4 y Col8
+    df_separado.drop(columns=['Col4', 'Col8'], inplace=True)
+
+    # Reordenar las columnas según el orden deseado
+    columnas_reordenadas = ['Col1', 'Col2', 'Col3', 'Col5', 'Col7', 'Col6']
+    df_separado = df_separado[columnas_reordenadas]
+
     # Guardar los datos en una nueva hoja
     with pd.ExcelWriter(archivo_excel, engine='openpyxl', mode='a') as writer:
         # Cambiar el nombre de la hoja si ya existe
@@ -23,10 +24,9 @@ def separar_datos_excel(archivo_excel, hoja_origen, hoja_destino):
             hoja_destino += 'nueva'  # nombre Hoja nueva
         df_separado.to_excel(writer, sheet_name=hoja_destino, index=False, header=False)
 
-
 # Llamada a la función con los parámetros deseados
 archivo_excel = 'C:\\Users\\krypto\\Desktop\\data.xlsx'
 hoja_origen = 'Hoja1'
-hoja_destino = '19-ASO-prueba3'
+hoja_destino = '19-ASO-prueba6'
 
 separar_datos_excel(archivo_excel, hoja_origen, hoja_destino)
